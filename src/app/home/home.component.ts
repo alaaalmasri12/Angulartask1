44f8118a6c70b6eaed215a7d7e927d7348f84431
alaaalmasri12 <alaa_almasri1997@yahoo.com>
2023-05-15 22:30:48 +0300

    Directives

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,13 @@ import { User } from '../user';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent  {
-user:User=
-{
-name:'alaa',
-age:25
-}
+  names: User[] = [
+    {name:"alaa",age:25,Salary:300},
+    {name:"rami",age:35,Salary:500},
+    {name:"ahmad",age:30,Salary:400},
+  ]
+name:string[]=["alaa","isam","mohammad"]
+test:string="active"
+num:number=2
+Salary:number= 3000;
 }
